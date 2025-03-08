54fced5c5bd892f609d75e655c3c9e79cbfb3d52
fadyashraf <f01550432980@gmail.com>
2025-03-08 22:18:35 +0200

    5 edition

--- app.py
+++ app.py
@@ -655,4 +655,4 @@ def logout():
     return redirect(url_for("home"))    
 if __name__ == "__main__":
     init_db()
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
